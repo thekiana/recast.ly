a1ef95ec6c8a0dd45b1c8d6926c38ebac631bea0
thekiana <kianakdonaldson@gmail.com>
2019-11-04 20:04:59 -0800

    Make app into stateless video component and rener components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,51 @@
-import VideoList from './components/VideoList.js';
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em><Search videos={this.state.videos} video={this.state.currentVideo} /></em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em><VideoPlayer videos={this.state.videos} video={this.state.currentVideo} /></em> view goes here</h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><em><VideoList videos={this.state.videos} video={this.state.currentVideo} callback={(e) => (this.handleClick(e))}/></em> view goes here</h5></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+
+  handleClick(e) {
+    this.setState({
+      currentVideo: e.target.value //selectedVideo from click;
+    });
+  }
+
+  // handleSearch() {
+  //   this.setState({
+  //     search: true;
+  //   });
+  // }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,16 +1,13 @@
-
 import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map((video, idx) => {
-      return (<VideoListEntry video={video} key={idx} />);
+      return (<VideoListEntry video={video} key={idx} onclick={props.callback}/>);
     })}
   </div>
-
-  // <div><h5><em>videoListEntry</em> view goes here</h5></div>
 );
-
+//<div><h5><em></em> view goes here</h5></div>
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
